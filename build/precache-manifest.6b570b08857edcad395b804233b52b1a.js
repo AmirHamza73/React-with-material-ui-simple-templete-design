b322a73b4291ad179182dc1561cb3c6b78ed94dc
amirhamza25 <77102421+amirhamza25@users.noreply.github.com>
2023-10-23 18:38:29 +0600

    first commit

---
+++ build/precache-manifest.6b570b08857edcad395b804233b52b1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848ed1933573ee42557e76aed5136d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da131c881dea27e56905",
+    "url": "/static/css/main.4fb989d8.chunk.css"
+  },
+  {
+    "revision": "398f031e5cc1b29547b7",
+    "url": "/static/js/2.817cf5e2.chunk.js"
+  },
+  {
+    "revision": "da131c881dea27e56905",
+    "url": "/static/js/main.b37637f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aaaceb4096bb5c8bd4827605365df9fb",
+    "url": "/static/media/image32.aaaceb40.png"
+  },
+  {
+    "revision": "923667c7934156a759320b7f2def00c6",
+    "url": "/static/media/image40.923667c7.png"
+  }
+]);
